90503a992c4fb6192ad583593babf5cca8f9839b
zishnu viknesh <shivakumar.zishnuviknesh@gmail.com>
2026-08-23 15:52:19 +0200

    Initial Commit

---
+++ AI_Smart/AI_SmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_SmartApp.swift
+//  AI_Smart
+//
+//  Created by Zishnu Viknesh on 23.08.26.
+//
+
+import SwiftUI
+
+@main
+struct AI_SmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI_Smart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI_Smart
+//
+//  Created by Zishnu Viknesh on 23.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
